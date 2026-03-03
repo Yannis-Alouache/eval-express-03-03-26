74d151df33a3cd8eb6cabb638fbe5441df2ab777
Félix Viart <viart.felix@gmail.com>
2026-03-03 11:03:15 +0100

    Added controller route + logic

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -1,5 +1,6 @@
 
-const { Ticket } = require('../models');
+const { Ticket, User} = require('../models');
+const {canTicketPassToStatus} = require("../utils/ticketUtils");
 
 // Create Ticket
 exports.createTicket = async (req, res) => {
@@ -65,3 +66,48 @@ exports.deleteTicket = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
+
+// Update ticket's status
+exports.editStatus = async (req, res) => {
+  try {
+    const ticket = await Ticket.findByPk(req.params.id);
+    if (!ticket) return res.status(404).json({ error: 'Ticket not found' });
+
+    const newStatus = req.query.status
+
+    if( !newStatus ) {
+      return res.status(400).json({
+        status: false,
+        error: "Please provide a status in order to change this ticket's status."
+      })
+    }
+
+    const currentUser = await User.findByPk(req.user.id);
+
+    if( !currentUser ) {
+      throw new Error("Please log in to edit that ticket's status.")
+    }
+
+    if( !canTicketPassToStatus(newStatus, ticket, currentUser) ) {
+      return res.status(409).json({
+        status: false,
+        error: `This ticket cannot pass to status '${newStatus}'.`
+      })
+    }
+
+    const updatedTicket = await ticket.update({
+      status: newStatus
+    })
+
+    res.status(200).json({
+      status: true,
+      message: "Ticket status updated successfully.",
+      ticket: updatedTicket
+    })
+  } catch (e) {
+    res.status(500).json({
+      status: false,
+      error: "Unknown error when updating this tickets' status. Please try again.",
+    })
+  }
+}
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -14,5 +14,7 @@ router.get('/:id', ticketController.getTicket);
 router.put('/:id', ticketController.updateTicket);
 // Delete ticket
 router.delete('/:id', ticketController.deleteTicket);
+// Edit a ticket's status
+router.put("/:id/status", ticketController.editStatus)
 
 module.exports = router;
